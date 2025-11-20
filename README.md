11bc7d9e3df3a06a63b64ca908b5469bb2bcc972
Hai Lang <hailn@trobz.com>
2025-11-20 14:52:42 +0700

    [IMP] F#T66647 new backend, --model option

--- README.md
+++ README.md
@@ -14,10 +14,12 @@ Run the above command to run the agent with instruction file name and default `c
 python agent.py <instruction file name> --backend <agent name>
 ```
 
-Run the above command to run the agent with instruction file name and agent name. Available agents: `codex`, `opencode`.
+Run the above command to run the agent with instruction file name and agent name. Available agents: `codex`, `opencode`, and `gemini`.
 
 ```bash
 python agent.py <instruction file name> --mode <mode>
 ```
 
 Run the above command to run the agent with instruction file name and mode. Available modes: `normal`, `yolo`. Mode `yolo` is to run `codex` with `--dangerously-bypass-approvals-and-sandbox` option.
+
+Add `--model` option to use a different model.
--- agent.py
+++ agent.py
@@ -3,22 +3,67 @@
 import typer
 
 from common import run
+from enum import Enum
+from typing_extensions import Annotated
 
 
-def main(instructions_file: str, backend: str = 'codex', mode: str = 'normal'):
-    with open(instructions_file, 'r') as file:
-        instructions = file.read()
+class Backend(str, Enum):
+    codex = 'codex'
+    opencode = 'opencode'
+    gemini = 'gemini'
+
+
+class Mode(str, Enum):
+    normal = 'normal'
+    yolo = 'yolo'
+
+
+def main(
+    instructions: Annotated[str,  typer.Argument(
+        help='Instruction to execute, required if instructions file'
+            ' is not provided',
+    )] = '',
+    backend: Backend = Backend.codex,
+    mode: Mode = Mode.normal,
+    instructions_file: Annotated[str, typer.Option(
+        '--instructions-file', '-f',
+        help='Instructions file, will override instructions argument,'
+            ' required if instructions is not provided',
+    )] = '',
+    model: Annotated[str, typer.Option(
+        '--model', '-m', help='Model to use',
+    )] = '',
+):
+    assert instructions_file or instructions, (
+        'Either instructions_file or instructions must be provided.'
+    )
+    if instructions_file:
+        with open(instructions_file, 'r') as file:
+            instructions = file.read()
     if backend == 'codex':
-        cmd_args = [
-            'codex', '--full-auto', 'exec', instructions,
-            '--skip-git-repo-check', '--sandbox', 'danger-full-access',
-        ]
+        cmd_args = ['codex', '--full-auto']
+        if model:
+            cmd_args.extend(['--model', model])
+        cmd_args.extend(['exec', instructions, '--skip-git-repo-check'])
         if mode == 'yolo':
             cmd_args.append('--dangerously-bypass-approvals-and-sandbox')
+        else:
+            cmd_args.extend(['--sandbox', 'danger-full-access'])
     elif backend == 'opencode':
+        if not model:
+            model = 'opencode/grok-code'
         cmd_args = [
-            'opencode', '--model', 'opencode/grok-code', 'run', instructions,
+            'opencode', '--model', model, 'run', instructions,
         ]
+    elif backend == 'gemini':
+        cmd_args = ['gemini']
+        if model:
+            cmd_args.extend(['--model', model])
+        if mode == 'yolo':
+            cmd_args.append('--yolo')
+        else:
+            cmd_args.extend(['--approval-mode', 'auto-approve'])
+        cmd_args.append(instructions)
     run('.', *cmd_args)
 
 
